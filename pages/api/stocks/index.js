e0a659223078018f3a8deabb5864be5ae7d97f02
digitribesthlm <patrik@makeablesthlm.se>
2025-04-24 16:46:23 +0200

    Next

--- pages/api/stocks/index.js
+++ pages/api/stocks/index.js
@@ -5,7 +5,7 @@ const dbName = process.env.MONGODB_DB;
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
-    return res.status(405).json({ error: 'Method not allowed' });
+    return res.status(405).json({ error: 'Method not allowed.' });
   }
 
   const client = await MongoClient.connect(uri);
